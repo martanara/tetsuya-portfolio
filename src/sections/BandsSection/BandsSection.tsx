f9fffd3f73be1f253c3811d5dac70298c5207d6a
Marta Nara <marta.nara@branchspace.com>
2024-11-09 14:56:13 +0100

    Fix bands section

--- src/sections/BandsSection/BandsSection.tsx
+++ src/sections/BandsSection/BandsSection.tsx
@@ -10,15 +10,27 @@ const BandsSection = () => {
     return (
         <Section title="Music" container>
             <div className="bands-wrapper">
-                {bandData.map((band: IBandData) => (
-                    <a href={`/music/${band.slug}`} key={band.id} className="band" data-aos="zoom-in">
-                        <div className="icon-wrapper"><img className="icon" src={`${process.env.PUBLIC_URL}/icons/${band.icon}`} alt="amp" /></div>
-                        <div className="title"><h3>{band.name}</h3></div>
-                        <div className="text">
-                            <p>{band.description}</p>
-                            <img className="band-photo" src={`${process.env.PUBLIC_URL}/images/${band.photo}`} alt="acid-sitter" />
+                {bandData.reduce((rows: JSX.Element[][], band: IBandData, index: number) => {
+                    if (index % 3 === 0) rows.push([]);
+                    rows[rows.length - 1].push(
+                        <div key={band.id} className="band">
+                            <div className="icon-wrapper">
+                                <img className="icon" src={`${process.env.PUBLIC_URL}/icons/${band.icon}`} alt="amp" />
+                            </div>
+                            <a href={`/music/${band.slug}`} className="title">
+                                <h3>{band.name}</h3>
+                            </a>
+                            <div className="text">
+                                <p>{band.description}</p>
+                                <img className="band-photo" src={`${process.env.PUBLIC_URL}/images/${band.photo}`} alt="acid-sitter" />
+                            </div>
                         </div>
-                    </a>
+                    );
+                    return rows;
+                }, []).map((row, rowIndex) => (
+                    <div className="band-row" key={rowIndex} data-aos="zoom-in">
+                        {row}
+                    </div>
                 ))}
             </div>
         </Section>
